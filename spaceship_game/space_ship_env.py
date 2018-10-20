d43a0bc7734e688f9a09b12b9755065c3621e126
Lin Xu <Lin.Xu@campus.lmu.de>
2018-10-20 18:43:33 +0200

    cur_state debugged

--- spaceship_game/space_ship_env.py
+++ spaceship_game/space_ship_env.py
@@ -5,13 +5,23 @@ import matplotlib.pyplot as plt
 class Environment:
     def __init__(self):
         self.board = np.zeros((6, 6))
-        self.finite_state = self.start_state = None
+        self.finite_state = self.start_state = self.cur_state = None
 
     def board_reset(self):
         self.board = np.zeros((6, 6))
+        self.cur_state = self.start_state
 
     def get_cur_board(self):
         return self.board
+
+    def set_cur_state(self, cur_state):
+        self.cur_state = cur_state
+
+    def get_cur_state(self):
+        if self.cur_state is None:
+            return self.start_state
+        else:
+            return self.cur_state
 
     def env_update(self, cur_state):
         self.board[cur_state[0], cur_state[1]] = 1
@@ -59,7 +69,7 @@ class EnvironmentGame(Environment):
         self.board[self.start_state] = 1
 
     def board_reset(self):
-        self.board = np.zeros((6, 6))
+        super(EnvironmentGame, self).board_reset()
         self.finite_state = (0, 5)
         self.start_state = (5, 1)
         self.board[self.finite_state] = 2
@@ -69,6 +79,7 @@ class EnvironmentGame(Environment):
         if action == "teleportation":
             next_state = [self.finite_state[0], self.finite_state[1]]
         else:
+            # print(cur_state)
             next_state = (np.array(cur_state) + np.array(action)).tolist()
             if next_state[0] < 0 or next_state[0] > (self.board.shape[0] - 1) or next_state[1] < 0 or next_state[1] > \
                     (self.board.shape[1] - 1):
--- spaceship_game/space_ship_tutorial.py
+++ spaceship_game/space_ship_tutorial.py
@@ -1,10 +1,13 @@
 from space_ship_env import EnvironmentTutorial
 
 
-def simple_tasks(success, env, steps, actions):
+def simple_tasks(success, env, steps, actions, jump_out):
 
-    cur_state = env.start_state
-    env.board_reset()
+    if jump_out:
+        cur_state = env.get_cur_state()
+    else:
+        env.board_reset()
+        cur_state = env.start_state
     env.show_board()
 
     for i in range(steps):
@@ -17,6 +20,7 @@ def simple_tasks(success, env, steps, actions):
                 print(f"{steps -i -1} move(s) left")
                 new_state = env.step(cur_state, actions[1])
                 cur_state = new_state
+                env.set_cur_state(cur_state)
                 board = env.env_update(cur_state)
                 # env.show_board()
                 if env.done(cur_state):
@@ -24,12 +28,15 @@ def simple_tasks(success, env, steps, actions):
                     success = True
                     break
                 print(board)
+                if jump_out:
+                    break
 
             elif trial == "t":
                 print("you got the skill to move your space ship to the right")
                 print(f"{steps - i - 1} move(s) left")
                 new_state = env.step(cur_state, actions[0])
                 cur_state = new_state
+                env.set_cur_state(cur_state)
                 board = env.env_update(cur_state)
                 # env.show_board()
                 if env.done(cur_state):
@@ -37,9 +44,13 @@ def simple_tasks(success, env, steps, actions):
                     success = True
                     break
                 print(board)
+                if jump_out:
+                    break
 
             else:
                 print(f"error! {steps -i -1} move(s) left.")
+                if jump_out:
+                    break
         else:
             raise ValueError
     return success
@@ -50,17 +61,18 @@ def main():
         "try to type a key from [a-z] to find out how to control the app, you have 10 chances to move your spaceship!")
     success = False
     env = EnvironmentTutorial()
+    board = env.board
 
     actions = [(0, 1), (-1, 0)]  # key "e" for action (0, 1) right, key "t" for action (-1, 0) up
     # states = ((x, y) for x in range(board.shape[0]) for y in range(board.shape[1]))
     steps = 10
 
-    simple_tasks(success, env, steps, actions)
+    simple_tasks(success, env, steps, actions, jump_out=False)
 
     while not success:
         try_again = str(input("try again [y] or see the answer[n]"))
         if try_again == "y":
-            success = simple_tasks(success, env, steps, actions)
+            success = simple_tasks(success, env, steps, actions, jump_out=False)
             # TODO: maybe call reward for trying out new things?
         elif try_again == "n":
             print("the answer: up: e, right t")
