27d7498b7b0fb3c0fe63e66d56c21978a95637c6
Lin Xu <Lin.Xu@campus.lmu.de>
2018-11-29 18:40:55 +0100

    add build

---
+++ spaceship_game/build/precache-manifest.dfecd320abee22b9546cdc8722e8af6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a23070d2766be35678e0050080f71c",
+    "url": "/static/media/spaceship.57a23070.svg"
+  },
+  {
+    "revision": "13559345540580c0a952cfaaf7bc7924",
+    "url": "/static/media/positive_sound.13559345.mp3"
+  },
+  {
+    "revision": "dad0bcc7ca80b65320d2df000b658771",
+    "url": "/static/media/negative_sound.dad0bcc7.mp3"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97713940b65ae33bef3a",
+    "url": "/static/js/main.97713940.chunk.js"
+  },
+  {
+    "revision": "a0ae417263c82af365f6",
+    "url": "/static/js/1.a0ae4172.chunk.js"
+  },
+  {
+    "revision": "97713940b65ae33bef3a",
+    "url": "/static/css/main.ad4badd5.chunk.css"
+  },
+  {
+    "revision": "ba02379dd410d330a5bc756a6712a34d",
+    "url": "/index.html"
+  }
+];
