2a42516bb7947daee51ff3a1a2092ae661471168
Lin Xu <Lin.Xu@campus.lmu.de>
2018-11-30 11:10:45 +0100

    Updates

---
+++ precache-manifest.a32d1ed63dfb08ea029d9fcb57cf61a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a23070d2766be35678e0050080f71c",
+    "url": "/dppr/static/media/spaceship.57a23070.svg"
+  },
+  {
+    "revision": "13559345540580c0a952cfaaf7bc7924",
+    "url": "/dppr/static/media/positive_sound.13559345.mp3"
+  },
+  {
+    "revision": "dad0bcc7ca80b65320d2df000b658771",
+    "url": "/dppr/static/media/negative_sound.dad0bcc7.mp3"
+  },
+  {
+    "revision": "3d76a1e6a0bfdbacd03e",
+    "url": "/dppr/static/js/runtime~main.3d76a1e6.js"
+  },
+  {
+    "revision": "55427731df4a4248308a",
+    "url": "/dppr/static/js/main.55427731.chunk.js"
+  },
+  {
+    "revision": "83e3914248a5b3922160",
+    "url": "/dppr/static/js/1.83e39142.chunk.js"
+  },
+  {
+    "revision": "55427731df4a4248308a",
+    "url": "/dppr/static/css/main.ad4badd5.chunk.css"
+  },
+  {
+    "revision": "7848934568669f103f4e4154cb797f62",
+    "url": "/dppr/index.html"
+  }
+];
